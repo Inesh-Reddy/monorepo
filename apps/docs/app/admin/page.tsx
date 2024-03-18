e299c72d9715b40f263543a3eb9cf7334a8d320d
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-18 20:56:06 +0530

    adding components through UI and turbo-generator

---
+++ apps/docs/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import Admin from "@repo/ui/Admin";
+
+export default function(){
+    return <div>
+        Hello from web app
+        <Admin />
+    </div>
+}
---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import Admin from "@repo/ui/Admin";
+import { InputBox } from "@repo/ui/input-box";
+
+export default function(){
+    return <div>
+        Hello to admin Page
+        <Admin />
+        <InputBox />
+    </div>
+}
